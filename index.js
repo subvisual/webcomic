bf2460248f906fe4c9e8eb4e091e0f2b41964cae
João Justo <jpjustonunes@gmail.com>
2015-11-26 20:39:21 +0000

    Adds audio logic

--- index.js
+++ index.js
@@ -29,3 +29,30 @@ $(function () {
     scene2,
   ]);
 });
+
+function audio() {
+  var audio = new Audio('audio/creaking-door-1.mp3');
+  window.subvisualAudio = audio;
+
+  function playAudio(event) {
+    audio.play();
+  }
+
+  function stopAudio(event) {
+    audio.pause();
+  }
+
+  // init controller
+  var controller = new ScrollMagic.Controller();
+
+  // create a scene
+  new ScrollMagic.Scene({
+    triggerElement: '#trigger',
+    duration: 50,  // the scene should last for a scroll distance of 100px
+    offset: -100      // start this scene after scrolling for 50px
+  })
+    .on("start", playAudio)
+    .on("end", stopAudio)
+    .addIndicators()
+    .addTo(controller); // assign the scene to the controller
+}
