375fb22060afb05311c0c9834700062bf972ebef
Bruno Azevedo <azevedo.252@gmail.com>
2015-11-28 21:10:00 +0000

    Second bubble

--- index.js
+++ index.js
@@ -2,29 +2,31 @@ $(function () {
   var $document = $(document);
   var $hero = $('#hero');
   var $bubble1 = $('#bubble-1');
-  var $bubble2 = $('#bubble0');
-  var controller = new ScrollMagic.Controller();
+  var $bubble2 = $('#bubble-2');
+  var $scene2Duration = $hero.height()*2;
+
+  var bubblesDuration = 600;
 
   var enterBubble1 = new TimelineMax();
   enterBubble1
-    .to($bubble1, 0.5, { x: -$bubble1.width() * 1.2}, '0');
+    .to($bubble1, 0.5, { x: -$bubble1.width() * 1.2 });
 
-  // var enterBubble2 = new TimelineMax();
-  // enterBubble2
-  //   .to($bubble2, 0.5, { x: -$bubble1.width() * 1.2}, '0')
-  //   .to($bubble2, 0.5, { y: $bubble1.height() + 100}, '0.5');
+  var enterBubble2 = new TimelineMax();
+  enterBubble2
+    .to($bubble2, 0.5, { x: -$bubble1.width() * 1.2 })
+    .to($bubble2, 0.5, { y: $bubble1.height() }, '0.5' );
 
   var scene = new ScrollMagic.Scene({
-    triggerElement: "#pinned-trigger1",
+    triggerElement: "#main-act",
     duration: $document.height() + $hero.height(),
     triggerHook: "onLeave",
   })
-  .setPin("#pinned-element1")
+  .setPin("#frame-1-wrapper")
   .addIndicators();
 
   var scene2 = new ScrollMagic.Scene({
-    triggerElement: "#pinned-trigger1",
-    duration: '80%',
+    triggerElement: "#main-act",
+    duration: $scene2Duration,
     triggerHook: 0.4,
   })
   .setTween("#frame-1", {
@@ -37,53 +39,27 @@ $(function () {
   .addIndicators();
 
   var scene3 = new ScrollMagic.Scene({
-    offset: 650,
-    triggerElement: "#pinned-trigger1",
-    duration: 100,
+    triggerElement: "#main-act",
+    offset: $scene2Duration,
+    duration: bubblesDuration,
     triggerHook: 0.4,
   })
   .setTween(enterBubble1)
   .addIndicators();
 
-  // var scene4 = new ScrollMagic.Scene({
-  //   offset: 700,
-  //   triggerElement: "#pinned-trigger1",
-  //   duration: 100,
-  //   triggerHook: 0.4,
-  // })
-  // .setTween(enterBubble2)
-  // .addIndicators();
+  var scene4 = new ScrollMagic.Scene({
+    triggerElement: "#main-act",
+    offset: $scene2Duration,
+    duration: bubblesDuration * 2,
+    triggerHook: 0.4,
+  })
+  .setTween(enterBubble2)
+  .addIndicators();
 
-  controller.addScene([
+  new ScrollMagic.Controller().addScene([
     scene,
     scene2,
     scene3,
+    scene4,
   ]);
 });
-
-function audio() {
-  var audio = new Audio('audio/creaking-door-1.mp3');
-  window.subvisualAudio = audio;
-
-  function playAudio(event) {
-    audio.play();
-  }
-
-  function stopAudio(event) {
-    audio.pause();
-  }
-
-  // init controller
-  var controller = new ScrollMagic.Controller();
-
-  // create a scene
-  new ScrollMagic.Scene({
-    triggerElement: '#trigger',
-    duration: 50,  // the scene should last for a scroll distance of 100px
-    offset: -100      // start this scene after scrolling for 50px
-  })
-    .on("start", playAudio)
-    .on("end", stopAudio)
-    .addIndicators()
-    .addTo(controller); // assign the scene to the controller
-}
