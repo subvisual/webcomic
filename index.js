9805e92e3c4a0dbffc5c35c005249907dcf045c4
Bruno Azevedo <azevedo.252@gmail.com>
2015-11-29 16:14:47 +0000

    Some cleaning

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ $(function () {
   var $bubble1 = $('#bubble-1');
   var $bubble2 = $('#bubble-2');
   var $scene2Duration = $hero.height()*2;
+  var bubble1Duration = 600;
+  var bubble2Duration = bubble1Duration * 2;
 
-  var bubblesDuration = 600;
+  var controller = new ScrollMagic.Controller();
+
+  var enterFrame1 = new TweenMax("#frame-1", 0.5, {
+    css: {
+      scale: '1.5%',
+      y: '150px',
+    },
+    ease: Linear.easeNone,
+  });
 
   var enterBubble1 = new TimelineMax();
   enterBubble1
@@ -22,44 +32,35 @@ $(function () {
     triggerHook: "onLeave",
   })
   .setPin("#frame-1-wrapper")
-  .addIndicators();
+  .addIndicators()
+  .addTo(controller);
 
   var scene2 = new ScrollMagic.Scene({
     triggerElement: "#main-act",
     duration: $scene2Duration,
     triggerHook: 0.4,
   })
-  .setTween("#frame-1", {
-    css: {
-      scale: '1.5%',
-      y: '150px',
-    },
-    ease: Linear.easeNone,
-  })
-  .addIndicators();
+  .setTween(enterFrame1)
+  .addIndicators()
+  .addTo(controller);
 
   var scene3 = new ScrollMagic.Scene({
     triggerElement: "#main-act",
     offset: $scene2Duration,
-    duration: bubblesDuration,
+    duration: bubble1Duration,
     triggerHook: 0.4,
   })
   .setTween(enterBubble1)
-  .addIndicators();
+  .addIndicators()
+  .addTo(controller);
 
   var scene4 = new ScrollMagic.Scene({
     triggerElement: "#main-act",
     offset: $scene2Duration,
-    duration: bubblesDuration * 2,
+    duration: bubble2Duration,
     triggerHook: 0.4,
   })
   .setTween(enterBubble2)
-  .addIndicators();
-
-  new ScrollMagic.Controller().addScene([
-    scene,
-    scene2,
-    scene3,
-    scene4,
-  ]);
+  .addIndicators()
+  .addTo(controller);
 });
