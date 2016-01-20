05ef24253d672cf9cb8fd3156faf1cac20cb2a5e
João Justo <jpjustonunes@gmail.com>
2016-01-20 02:00:54 +0000

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 $(function () {
   var $hero = $('#hero');
+  var $bubble = $('#bubble1');
+  var viewport = { width: $(document).width(), height: $(document).height()}
+
   var controller = new ScrollMagic.Controller();
 
   var scene = new ScrollMagic.Scene({
     triggerElement: "#pinned-trigger1",
-    duration: $(document).height() + $hero.height(),
+    duration: '100%',
     triggerHook: "onLeave",
   })
   .setPin("#pinned-element1")
@@ -12,7 +15,7 @@ $(function () {
 
   var scene2 = new ScrollMagic.Scene({
     triggerElement: "#pinned-trigger1",
-    duration: '200%',
+    duration: '100%',
     triggerHook: 0.4,
   })
   .setTween("#frame-1", {
@@ -24,35 +27,61 @@ $(function () {
   })
   .addIndicators();
 
+  var scene3 = new ScrollMagic.Scene({
+    triggerElement: "#frame-1",
+    offset: 900,
+    duration: '300px',
+    triggerHook: 0.4,
+  })
+  .setTween('#bubble-1', {
+    css: {
+      left: '85%'
+    },
+    ease: Linear.easeNone,
+  })
+  .addIndicators();
+
+  var scene4 = new ScrollMagic.Scene({
+    triggerElement: "#frame-1",
+    offset: 1300,
+    duration: '200px',
+    triggerHook: 0.4,
+  })
+  .setTween('#bubble-2', {
+    css: {
+      top: '38%',
+      opacity: '1'
+    },
+    ease: Linear.easeNone,
+  })
+  .addIndicators();
+
+  var scene5 = new ScrollMagic.Scene({
+    triggerElement: "#frame-1",
+    offset: 1900,
+    duration: '200px',
+    triggerHook: 0.4,
+  })
+  .setTween('#bubble-2', {
+    css: {
+      top: '108%',
+      opacity: '0'
+    },
+    ease: Linear.easeNone,
+  })
+  .addIndicators();
+
   controller.addScene([
     scene,
     scene2,
+    scene3,
+    scene4,
+    scene5
   ]);
+
+  // var div = $('div');
+  // setInterval(function(){
+  //   var pos = div.scrollTop();
+  //   div.scrollTop(pos + 10);
+  // }, 200)
 });
-
-function audio() {
-  var audio = new Audio('audio/creaking-door-1.mp3');
-  window.subvisualAudio = audio;
-
-  function playAudio(event) {
-    audio.play();
-  }
-
-  function stopAudio(event) {
-    audio.pause();
-  }
-
-  // init controller
-  var controller = new ScrollMagic.Controller();
-
-  // create a scene
-  new ScrollMagic.Scene({
-    triggerElement: '#trigger',
-    duration: 50,  // the scene should last for a scroll distance of 100px
-    offset: -100      // start this scene after scrolling for 50px
-  })
-    .on("start", playAudio)
-    .on("end", stopAudio)
-    .addIndicators()
-    .addTo(controller); // assign the scene to the controller
-}
